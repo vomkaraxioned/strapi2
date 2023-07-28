86275e3a02bd2c25a4a1bb76204fb9b60368b9ce
prdxn88 <omkarv@axioned.com>
2023-07-28 18:53:59 +0530

    created api for update

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
+  proxy: true,
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('https://strapi-7mwt.onrender.com'), // Sets the public URL of the application.
   app: {
     keys: env.array('APP_KEYS'),
   },
